2b4a2c93fc180177ac3bc5c0cd06e685d046be37
Paxius025 <pantongbk@gmail.com>
2025-07-10 18:30:52 +0700

    fix: correct volume path for backend-web service in docker-compose.yml

--- backend-web/index.js
+++ backend-web/index.js
@@ -1,21 +1,25 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import databaseRoutes from './src/routes/database.routes.js';
-
-dotenv.config({quiet: true});
+import express from "express";
+import dotenv from "dotenv";
+import databaseRoutes from "./src/routes/database.routes.js";
+import { errorHandler } from "./src/middleware/errorHandler.js";
+dotenv.config({ quiet: true });
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello from the backend web service!');
+app.get("/", (req, res) => {
+  res.send("Hello from the backend web service!");
 });
 
-app.use('/api/database', databaseRoutes);
+app.use("/api/database", databaseRoutes);
+
+app.use(errorHandler);
 
 const PORT = process.env.BACKEND_NODE_PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Backend web service is running on port http://localhost:${PORT}`);
+  console.log(
+    `Backend web service is running on port http://localhost:${PORT}`
+  );
 });
